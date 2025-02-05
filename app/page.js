78ff114767da5146995772e514de49c8152e81f4
Harshil2103 <harshilvijaykumar.rathod@edu.sait.ca>
2025-02-05 10:32:25 -0700

    remaned the folder

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Page() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
         <Link href="week2">week 2 Assignment</Link><br></br>
-        <Link href="week3">week 3 assignment</Link>
+        <Link href="week-3">week 3 assignment</Link>
       </p>
     </main>
   );
